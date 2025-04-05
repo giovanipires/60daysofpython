0985389424c24b9c99b8a190fa1ba2c04e5641d8
Giovani Pires <giovani.pires@gmail.com>
2025-04-05 13:52:16 -0300

    50º dia de Python, problemas ao instalar o PyAutoGui.

---
+++ day50/automate_clicks.py
@@ -0,0 +1,9 @@
+try:
+    import PyAutoGUI
+    print("PyAutoGUI está instalado! Versão:", PyAutoGUI.__version__)
+except ImportError:
+    print("PyAutoGUI NÃO está instalado.")
+
+# posicao = pyautogui.position()
+
+# print(posicao)
