88293a375f9f1c09e64089470c576210e67a391f
Giovani Pires <giovani.pires@gmail.com>
2025-04-28 19:28:34 -0300

    Continuidade nas estruturas de controle, criando um switch com função e dicionário.

---
+++ day56/switch_02.py
@@ -0,0 +1,19 @@
+def get_dia_semana(dia):
+    dias = {
+        1: 'Domingo',
+        2: 'Segunda-feira',
+        3: 'Terça-feira',
+        4: 'Quarta-feira',
+        5: 'Quinta-feira',
+        6: 'Sexta-feira',
+        7: 'Sábado'
+    }
+    return dias.get(dia, '** inválido **')
+
+if __name__ == '__main__':
+    for dia in range(1,8):
+        if dia > 1 and dia < 7:
+            print(f'Dia útil: {dia} - {get_dia_semana(dia)}')
+        else:
+            print(f'Fim de semana: {dia} - {get_dia_semana(dia)}')
+    
